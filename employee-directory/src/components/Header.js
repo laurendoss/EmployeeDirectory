fd532bd21c1185aa3c7a16a70cf84f902d6ce490
Lauren Doss <doss.laurene@gmail.com>
2020-05-28 16:42:24 -0700

    got header to fit width of browser page

--- employee-directory/src/components/Header.js
+++ employee-directory/src/components/Header.js
@@ -4,14 +4,14 @@ import Jumbotron from 'react-bootstrap/Jumbotron'
 
 function Header(){
     return(
-        <div className="container">
+       
         <Jumbotron className="jumbotron">
           <h1>Employee Directory</h1>
           {/* <p>
             <button className="btn btn-primary btn-lg">Learn more</button>
           </p> */}
         </Jumbotron>
-        </div>
+      
        
     )
 }
