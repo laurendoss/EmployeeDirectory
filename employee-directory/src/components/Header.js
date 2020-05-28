2a7cd2bec8d6197ab4b6af1444d8c051a7e6173b
Lauren Doss <doss.laurene@gmail.com>
2020-05-28 16:39:25 -0700

    styled header

--- employee-directory/src/components/Header.js
+++ employee-directory/src/components/Header.js
@@ -1,16 +1,18 @@
 import React from 'react'; 
-import "../styles/style.css"; 
+import "../styles/Header.css"; 
+import Jumbotron from 'react-bootstrap/Jumbotron'
 
 function Header(){
     return(
         <div className="container">
-        <div className="jumbotron">
+        <Jumbotron className="jumbotron">
           <h1>Employee Directory</h1>
           {/* <p>
             <button className="btn btn-primary btn-lg">Learn more</button>
           </p> */}
+        </Jumbotron>
         </div>
-        </div>
+       
     )
 }
 
